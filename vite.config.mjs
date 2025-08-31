1835d91a6c573f3c15652c3b436d4b9a0ada1889
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2025-08-31 19:37:24 +0100

    del portfolio

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        portfolio: resolve(__dirname, "portfolio.html"),
       },
     },
   },
